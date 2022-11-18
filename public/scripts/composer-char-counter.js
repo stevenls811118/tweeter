a3a421b7484c3a10bbbfb3db8f8e8644abe4b4d5
Yifan Shi <stevenls1118@msn.com>
2022-11-17 23:54:31 -0700

    add counter to the tweeter form, modify index.html and new-tweet.css

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,32 @@
-$(document).ready(function() {
-  // --- our code goes here ---
-  console.log('Load successfully');
+
+console.log('Load successfully');
+
+
+// Retrive the form, tweet text
+const $form = $('#tweet-form');
+const $tweet = $('#tweet-text');
+const $counter = $('#counter');
+
+console.log($form, $tweet, $counter);
+
+// Listen for form submissions
+
+$tweet.on('input', function(event) {
+  $counter.empty(); // Clear out the old counter
+  let counter = 140 - this.value.length;
+  
+  if (counter >= 0) { // If exceed 140 character limit, counter should be red
+    $counter.append(`<a>${counter}</a>`);
+  } else {
+    $counter.append(`<a style="color: red">${counter}</a>`);
+  }
+
 });
+
+
+// $form.on('submit', (event) => {
+//   event.preventDefault(); // Stop the form from loading a new page
+
+//   const tweetText = $tweet.val();
+//   console.log(tweetText);
+// });
